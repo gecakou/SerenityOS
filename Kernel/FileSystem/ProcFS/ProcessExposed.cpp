spholz/serenity

Kernel/MM: Replace Region::Cacheable with a more generic MemoryType enum
This replaces all usages of Cacheable::Yes with MemoryType::Normal and
Cacheable::No with either MemoryType::NonCacheable or MemoryType::IO,
depending on the context.

The Page{Directory,Table}::set_cache_disabled function therefore also
has been replaced with a more appropriate set_memory_type_function.
Adding a memory_type "getter" would not be as easy, as some
architectures may not support all memory types, so getting the memory
type again may be a lossy conversion. The is_cache_disabled function
was never used, so just simply remove it altogether.

There is no difference between MemoryType::NonCacheable and
MemoryType::IO on x86 for now.

Other architectures currently don't respect the MemoryType at all.

Kernel/FileSystem/ProcFS/ProcessExposed.cpp
@@ -311,7 +311,7 @@ ErrorOr<void> Process::procfs_get_virtual_memory_stats(KBufferBuilder& builder) 
             if (region.vmobject().is_anonymous()) {
                 TRY(region_object.add("volatile"sv, static_cast<Memory::AnonymousVMObject const&>(region.vmobject()).is_volatile()));
             }
-            TRY(region_object.add("cacheable"sv, region.is_cacheable()));
+            TRY(region_object.add("cacheable"sv, Memory::memory_type_to_string(region.memory_type())));
             TRY(region_object.add("address"sv, region.vaddr().get()));
             TRY(region_object.add("size"sv, region.size()));
             TRY(region_object.add("amount_resident"sv, region.amount_resident()));

gecakou/SerenityOS

Kernel/FileSystem/ProcFS/ProcessExposed.cpp
@@ -311,7 +311,7 @@ ErrorOr<void> Process::procfs_get_virtual_memory_stats(KBufferBuilder& builder) 
             if (region.vmobject().is_anonymous()) {
                 TRY(region_object.add("volatile"sv, static_cast<Memory::AnonymousVMObject const&>(region.vmobject()).is_volatile()));
             }
-            TRY(region_object.add("cacheable"sv, region.is_cacheable()));
+            TRY(region_object.add("memory_type"sv, Memory::memory_type_to_string(region.memory_type())));
             TRY(region_object.add("address"sv, region.vaddr().get()));
             TRY(region_object.add("size"sv, region.size()));
             TRY(region_object.add("amount_resident"sv, region.amount_resident()));